420a605499b98906603fecd5bcc59ea582f6b7db
Yurii Mitrohin <lordroads@mail.ru>
2023-01-19 12:28:25 +0300

    Итоговый проект. Часть 1

---
+++ Lesson-9/EmployeesWebApplication/ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,10 @@
+namespace EmployeesWebApplication.ViewModels;
+
+public class EmployeesViewModel
+{
+    public int Id { get; set; }
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string Patronymic { get; set; }
+    public DateTime Birthday { get; set; }
+}
